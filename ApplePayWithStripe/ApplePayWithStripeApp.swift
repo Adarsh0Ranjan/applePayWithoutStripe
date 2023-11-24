fc1ab420574f7871e31533535ee9163d9b908dd0
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2023-11-24 13:23:45 +0530

    Initial Commit

---
+++ ApplePayWithStripe/ApplePayWithStripeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePayWithStripeApp.swift
+//  ApplePayWithStripe
+//
+//  Created by Adarsh Ranjan on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ApplePayWithStripeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
